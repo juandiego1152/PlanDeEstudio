3b9009dd9fbfbd668634d6d67a6758c4d31039a6
Juan Diego <juandiego-96-@hotmail.com>
2017-06-20 17:18:03 -0500

    Algunas funciones de la clase OPTIONAL para manejar valores nulos

---
+++ Java8/proyectoJava/src/main/java/com/proyecto/java8/Optional/OptionalApp.java
@@ -0,0 +1,42 @@
+package com.proyecto.java8.Optional;
+
+import java.util.Optional;
+
+public class OptionalApp {
+	
+	public void probar(String valor){
+		
+		//System.out.println(valor.contains("mito"));
+		try {
+			Optional op = Optional.empty();
+			op.get();
+		} catch (Exception e) {
+			System.out.println("No hay tipo");
+		}
+		
+	}
+	
+	public void orElse(String valor){
+		Optional<String> op = Optional.ofNullable(valor);
+		String x = op.orElse("Predeterminado");
+		System.out.println(x);
+	}
+	
+	public void orElseThrow(String valor){
+		Optional<String> op = Optional.ofNullable(valor);
+		op.orElseThrow(NumberFormatException::new);
+	}
+	
+	public void isPresent(String valor){
+		Optional<String> op = Optional.ofNullable(valor);
+		System.out.println(op.isPresent());
+	
+	}
+	public static void main(String[] args) {
+			OptionalApp app = new OptionalApp();
+			//app.probar(null);
+			//app.orElse("juan");
+			//app.orElseThrow(null);
+			app.isPresent("juan");
+	}
+}
