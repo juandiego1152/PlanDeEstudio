1b9b32bf51fe4663f2d90266c3dd2f85e2a66c19
Juan Diego <juandiego-96-@hotmail.com>
2017-06-30 15:53:01 -0500

    Organizando el codigo de Logger en el ejemplo 14.

--- Java8/proyectoJava/src/main/java/com/proyecto/java8/CompletableFurute/CompletableFuture14.java
+++ Java8/proyectoJava/src/main/java/com/proyecto/java8/CompletableFurute/CompletableFuture14.java
@@ -5,32 +5,19 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.logging.*;
 
+import org.junit.runners.AllTests;
+
 public class CompletableFuture14 {
 
-	private final static Logger LOGGER = Logger.getLogger("Mylogger");
-	//private final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-	//private final static Logger LOGGER = Logger.getLogger(CompletableFuture14.class.getName());
+	
+	private final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 	
 	private static ExecutorService executor = Executors.newCachedThreadPool();
 	
-	public static void main(String[] args) {
-              
-        LOGGER.severe("Severe");
-        LOGGER.info("Info");
-        LOGGER.warning("Warning");
-        LOGGER.log(Level.SEVERE, "Severe 2");
-        LOGGER.log(Level.INFO, "Info 2");
-        LOGGER.log(Level.WARNING, "Warning 2");
-        //LOGGER.config("Config");
-        LOGGER.fine("Fine");
-        LOGGER.finer("Finer");
+	public static void main(String[] args) {	        
 		CompletableFuture14 app = new CompletableFuture14();
-		app.llamarMetodos();
-		
-    }
-	 
-	
-
+		app.llamarMetodos();		
+    } 
 	
 	 private void Sleep(int i) {
 		try{
@@ -40,27 +27,21 @@ public class CompletableFuture14 {
 			while (x < finalx) {
 				System.out.println(x);
 				Thread.sleep(1000);
-				x = x + 1000;
-				 LOGGER.fine("Fine");
-			        LOGGER.finer("Finer");
+				x = x + 1000;				 
 			}		
-		}catch(InterruptedException e){	}	
-		
+		}catch(InterruptedException e){	}			
 	}
 	 
-	 CompletableFuture<String> futureAsync = CompletableFuture.supplyAsync(() -> {
-		    //LOGGER.info("Comenzando supplyAsync for thenApply...");
-		 	//System.out.println("Algo nuevo");
+	 CompletableFuture<String> futureAsync = CompletableFuture.supplyAsync(() -> {	  
 		 	LOGGER.log(Level.INFO, "Comenzando supplyAsync for thenApply...");
 		    //Sleep.sleepSeconds(2);
-		    Sleep(2);
-		    //LOGGER.info("Terminado supplyAsync for thenApply!");
+		    Sleep(2);		  
 		    LOGGER.log(Level.INFO, "Terminado supplyAsync for thenApply!");
 		    return "Terminado";
 		}, executor);
 		 
 		CompletableFuture<String> futureApply = futureAsync.thenApplyAsync(s -> {
-			//System.out.println("Otra impresion");
+			//System.out.println("Comenzando applyAsync...");
 		    LOGGER.info("Comenzando applyAsync...");
 		    //Sleep.sleepSeconds(2);
 		    Sleep(2);
@@ -68,9 +49,7 @@ public class CompletableFuture14 {
 		    return s.toUpperCase();
 		}, executor);
 		 
-		public void llamarMetodos(){
-			//futureApply.whenCompleteAsync((s, e) -> LOGGER.info("Resultado applyAsync: {}", s));}
-			//futureApply.whenCompleteAsync((s, e) -> LOGGER.log(Level.WARNING ,"Resultado applyAsync: {}"+ s));		
+		public void llamarMetodos(){				
 			futureApply.whenCompleteAsync((s, e) -> LOGGER.info("Resultado applyAsync: {}" + s) );
 			//futureApply.whenCompleteAsync((s, e) ->  System.out.println("Resultado applyAsync: {}" + s) );
 		}
