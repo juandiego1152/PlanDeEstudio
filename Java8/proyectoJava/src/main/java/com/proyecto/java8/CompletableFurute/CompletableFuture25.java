3ff2c4bcdd58f1308f7a2e21414955836b0527ba
Juan Diego <juandiego-96-@hotmail.com>
2017-07-03 19:51:39 -0500

    Ejemplo completable future usando acceptEitherAsync y agregamos la clase Sleep

---
+++ Java8/proyectoJava/src/main/java/com/proyecto/java8/CompletableFurute/CompletableFuture25.java
@@ -0,0 +1,40 @@
+package com.proyecto.java8.CompletableFurute;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Logger;
+
+public class CompletableFuture25 {
+
+	private static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+	private static ExecutorService executor = Executors.newCachedThreadPool();
+	
+	  // acceptEither
+    CompletableFuture<String> future1 = CompletableFuture.supplyAsync(() -> {
+        LOGGER.info("Comenzando future1 for acceptEither...");
+        Sleep.sleepSeconds(3);
+        LOGGER.info("Terminado future1 for acceptEither!");
+        return "Segundo";
+    }, executor);
+
+    CompletableFuture<String> future2 = CompletableFuture.supplyAsync(() -> {
+        LOGGER.info("Comenzando future2 for acceptEither...");
+        Sleep.sleepSeconds(1);
+        LOGGER.info("Terminado future2 for acceptEither!");
+        return "Primero";
+    }, executor);
+
+    public void llamarMetodo(){
+    	future1.acceptEitherAsync(future2, (s) ->
+        LOGGER.info("En el acceptEither, recibido el primer resultado: {}"+ s)
+        	, executor);
+	}
+		
+	public static void main(String[] args) {
+		CompletableFuture25 app = new CompletableFuture25();
+		app.llamarMetodo();
+	}
+	
+    
+}
---
+++ Java8/proyectoJava/src/main/java/com/proyecto/java8/CompletableFurute/Sleep.java
@@ -0,0 +1,17 @@
+package com.proyecto.java8.CompletableFurute;
+
+public class Sleep {
+
+
+	public static void sleepSeconds(int i){
+		try{
+			int finalx = i * 1000;
+			int x = 0;
+			while (x < finalx) {
+				System.out.println(x);
+				Thread.sleep(1000);
+				x = x + 1000;				 
+			}		
+		}catch(InterruptedException e){	}		
+	}
+}
