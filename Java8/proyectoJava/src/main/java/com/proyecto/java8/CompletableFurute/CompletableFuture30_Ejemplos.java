fec27389b30bdd29c421f753b583729a40b7226e
Juan Diego <juandiego-96-@hotmail.com>
2017-07-05 11:30:48 -0500

    Ejemplo de completable future, llenar 2 listas, sumar cada una de estas listas y luego sumar ambas

---
+++ Java8/proyectoJava/src/main/java/com/proyecto/java8/CompletableFurute/CompletableFuture30_Ejemplos.java
@@ -0,0 +1,76 @@
+package com.proyecto.java8.CompletableFurute;
+
+import java.time.Year;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Logger;
+
+import javax.security.auth.kerberos.KerberosKey;
+import javax.security.auth.x500.X500Principal;
+
+
+public class CompletableFuture30_Ejemplos {
+
+	private static final Logger LOG = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
+	private static final ExecutorService execut = Executors.newCachedThreadPool();
+	
+	List<Double> lista1 = new ArrayList<Double>();
+	List<Double> lista2 = new ArrayList<Double>();
+	
+	public static void main(String[] args) {
+		CompletableFuture30_Ejemplos app = new CompletableFuture30_Ejemplos();
+		app.llamarMetodo();
+		
+	}
+	
+	public void llamarMetodo(){
+		CompletableFuture<Double> primeraLista = CompletableFuture.supplyAsync(() -> llenarLista1(), execut)
+				.thenApply(s -> sumarLista(s));
+		CompletableFuture<List<Double>> segundaLista = CompletableFuture.supplyAsync(() -> llenarLista2(), execut);
+		
+		CompletableFuture<Double> sumaSegundaLista = segundaLista.thenApply(s -> sumarLista(s));
+		
+		CompletableFuture<Void> restultado = primeraLista.thenCombineAsync(sumaSegundaLista, (x,y) -> x + y).thenAccept(s ->LOG.info("resultado: " + s));
+		
+		//LOG.info("Resultado " + restultado.get());
+						
+	}
+	
+	public double sumarLista(List<Double> lista){		
+		int resultado = lista.stream().mapToInt(X -> X.intValue()).sum();
+		//double resultado = lista.forEach(x -> x);
+		return resultado;		
+	}	
+			
+	public double suma(double x, double y){
+		double resultadoFinal = x + y;
+		return resultadoFinal;
+	}
+	
+	public List<Double> llenarLista1(){
+		for (int i = 0; i < 10; i++) {
+			lista1.add((Math.random())+1 );
+		}
+		lista1.forEach(System.out::println);
+		return lista1;
+	}
+	
+	public List<Double> llenarLista2(){		
+		lista2.add(2.0);
+		lista2.add(2.0);
+		lista2.add(2.0);
+		lista2.add(2.0);
+		lista2.add(2.0);
+		
+		lista2.add(3.0);
+		lista2.add(3.0);
+		lista2.add(3.0);
+		lista2.add(3.0);
+		lista2.add(3.0);
+		lista2.forEach(System.out::println);
+		return lista2;
+	}
+}
